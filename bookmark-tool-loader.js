ec1f9ad5ce517bca035d27d9fa7b88a14adcbe9f
yujiangshui <yujiangshui@gmail.com>
2015-01-06 11:51:09 +0800

    Add: load function

--- bookmark-tool-loader.js
+++ bookmark-tool-loader.js
@@ -1,3 +1,57 @@
 (function() {
 
+	// loadFile('jQuery URL', 'js', function() {
+	// 	loadFile('Your Plugin', 'js', function() {
+	// 		// initialize
+	// 	});
+	// });
+
+	/**
+	 * Load File
+	 * @param  {string} file   file url
+	 * @param  {string} type   file type (css or js)
+	 * @param  {function} handle file loaded callback
+	 */
+	function loadFile (file, type, handle) {
+		var head = document.getElementsByTagName('head')[0],
+			res;
+
+		// avoid load more times
+		if ( !window.loadedFiles ) {
+			window.loadedFiles = [];
+		}
+		for (var i = 0; i < window.loadedFiles.length; i++) {
+			if( file === window.loadedFiles[i] )
+				return;
+		}
+		window.loadedFiles.push(file);
+
+		// load res
+		if ( type === 'js' ) {
+			res = document.createElement('script');
+			res.src = file;
+		}else if ( type === 'css' ) {
+			res = document.createElement('link');
+			res.rel = 'stylesheet';
+			res.type = 'text/css';
+			res.href = file;
+		}
+		head.appendChild(res);
+
+		// callback
+		if (handle) {
+			if ( type === 'js' && document.all ) {
+				res.onreadystatechange = function() {
+					if ( js.readyState === 'loaded' || js.readyState === 'complete' ) {
+						handle();
+					}
+				};
+			} else {
+				res.onload = function() {
+					handle();
+				};
+			}
+		}
+	}
+
 })();
